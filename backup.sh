c73fb1535beb778dcf219c60b8b7abe3f54b9219
Jens Heine <jens@heine-bb.de>
2016-11-29 11:20:17 +0100

    Di 29. Nov 11:20:13 CET 2016

--- backup.sh
+++ backup.sh
@@ -11,6 +11,14 @@ myDate=`date`
 #		message=$1
 #fi
 
+echo -n "Is this a good question (y/n)? "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    echo "Ja bitte"
+else
+    echo "Nein danke"
+fi
+
 echo $myDate
 
 ## create and push commit
